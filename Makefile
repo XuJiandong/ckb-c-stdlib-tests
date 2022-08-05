fd1401ca2ab8798a8529f6ff5acf7cd64562f45a
xjd <lynndon@gmail.com>
2022-08-05 16:18:07 +0800

    Update

--- Makefile
+++ Makefile
@@ -2,11 +2,13 @@ TARGET := riscv64-unknown-linux-gnu
 CC := $(TARGET)-gcc
 LD := $(TARGET)-gcc
 OBJCOPY := $(TARGET)-objcopy
-CFLAGS := -fPIC -O3 -fno-builtin-printf -fno-builtin-memcmp -nostdinc -nostdlib -nostartfiles -fvisibility=hidden -fdata-sections -ffunction-sections -I deps/ckb-c-stdlib-dev -I deps/ckb-c-stdlib-dev/libc -I deps/ckb-c-stdlib-dev/molecule -Wall -Werror -Wno-nonnull -Wno-nonnull-compare -Wno-unused-function -g
+CFLAGS := -fPIC -O0 -fno-builtin-printf -fno-builtin-memcmp -nostdinc -nostdlib -nostartfiles -fvisibility=hidden -fdata-sections -ffunction-sections -I deps/ckb-c-stdlib-dev -I deps/ckb-c-stdlib-dev/libc -I deps/ckb-c-stdlib-dev/molecule -Wall -Werror -Wno-nonnull -Wno-nonnull-compare -Wno-unused-function -g
 LDFLAGS := -Wl,-static -fdata-sections -ffunction-sections -Wl,--gc-sections
 
 # docker pull nervos/ckb-riscv-gnu-toolchain:gnu-bionic-20191012
 BUILDER_DOCKER := nervos/ckb-riscv-gnu-toolchain@sha256:aae8a3f79705f67d505d1f1d5ddc694a4fd537ed1c7e9622420a470d59ba2ec3
+PORT := 9099
+
 
 all: build/test_malloc
 
@@ -22,6 +24,17 @@ fmt:
 run:
 	RUST_LOG=debug ckb-debugger --bin build/test_malloc
 
+run-debugger:
+	ckb-debugger --mode gdb --gdb-listen 127.0.0.1:${PORT} --bin build/test_malloc
+
+run-gdb:
+	riscv64-unknown-linux-gnu-gdb -ex "target remote 127.0.0.1:${PORT}" build/test_malloc
+
+run-docker:
+	docker run --net=host --rm -it -v `pwd`:/code ${BUILDER_DOCKER} bash
+
+clean:
+	rm -f build/*
 	
 install:
 	cargo install --git https://github.com/nervosnetwork/ckb-standalone-debugger
--- c/test_malloc.c
+++ c/test_malloc.c
@@ -1,4 +1,5 @@
 #define CKB_C_STDLIB_PRINTF
+#define CKB_C_STDLIB_MALLOC
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -7,36 +8,58 @@
 #include "ckb_syscalls.h"
 #include "helper.h"
 
-#ifndef CKB_BRK_MIN
-extern char _end[]; /* _end is set in the linker */
-#define CKB_BRK_MIN ((uintptr_t)&_end)
-#endif
-#ifndef CKB_BRK_MAX
-#define CKB_BRK_MAX 0x00300000
-#endif
+#define COUNT_OF(a) (sizeof(a) / sizeof(a[0]))
 
-void* _sbrk(uintptr_t incr) {
-    static uintptr_t p = CKB_BRK_MIN;
-    uintptr_t start = p;
-    p += incr;
-    if (p > CKB_BRK_MAX) {
-        return (void*)-1;
-    }
-    return (void*)start;
+void loop_free(void *array[], size_t array_length, size_t step) {
+  for (size_t i = 0; i < array_length; i += step) {
+    free(array[i]);
+    array[i] = 0;
+  }
+}
+
+void test_malloc() {
+  void *array[1024] = {0};
+  for (size_t i = 0; i < COUNT_OF(array); i++) {
+    array[i] = malloc(i * 4);
+    ASSERT(array[i] != 0);
+  }
+  loop_free(array, COUNT_OF(array), 2);
+  loop_free(array, COUNT_OF(array), 3);
+  loop_free(array, COUNT_OF(array), 4);
+  loop_free(array, COUNT_OF(array), 5);
+
+  void *array2[1024] = {0};
+  for (size_t i = 0; i < COUNT_OF(array); i++) {
+    array[i] = malloc(i * 4);
+    ASSERT(array[i] != 0);
+  }
+  loop_free(array2, COUNT_OF(array2), 1);
+
+  uint64_t p = (uint64_t)_sbrk(0);
+  printf("finally, the brk is at %lu K\n", p / 1024);
+  uint64_t max_brk = 3 * 1024 * 1024;
+  void *ptr = malloc(max_brk - p);
+  ASSERT_EQ(ptr, 0);
+
+  ptr = malloc(max_brk - p - 100);
+  ASSERT(ptr != 0);
+}
+
+void test_sbrk() {
+  uint64_t base = (uint64_t)_sbrk(0);
+  printf("_end = %lu K\n", base / 1024);
+  uint64_t ptr1 = (uint64_t)_sbrk(100);
+  uint64_t ptr2 = (uint64_t)_sbrk(200);
+  ASSERT_EQ(ptr2 - ptr1, 100);
+  uint64_t *ptr3 = _sbrk(8);
+  *ptr3 = 100;
+  *(ptr3 + 1) = 200;
+  uint64_t three_million = 3 * 1024 * 1024;
+  uint64_t overflow_ptr = (uint64_t)_sbrk(three_million);
+  ASSERT_EQ(overflow_ptr, -1);
 }
 
 int main() {
-    uint64_t base = (uint64_t)_sbrk(0);
-    printf("_end = %lu K\n", base / 1024);
-    uint64_t ptr1 = (uint64_t)_sbrk(100);
-    uint64_t ptr2 = (uint64_t)_sbrk(200);
-    ASSERT_EQ(ptr2 - ptr1, 100);
-    uint64_t* ptr3 = _sbrk(8);
-    *ptr3 = 100;
-    *(ptr3 + 1) = 200;
-    uint64_t three_million = 3 * 1024 * 1024;
-    uint64_t overflow_ptr = (uint64_t)_sbrk(three_million);
-    ASSERT_EQ(overflow_ptr, -1);
-
-    return 0;
+  test_malloc();
+  return 0;
 }
